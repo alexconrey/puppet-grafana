32ca957f5d020758c84cf3be54b25030a8cbf28f
Alex Conrey <alex.conrey@wellspring.com>
2019-04-05 13:42:13 -0500

    add required parameters for grafana objects

--- lib/puppet/type/grafana_folder.rb
+++ lib/puppet/type/grafana_folder.rb
@@ -12,4 +12,43 @@ Puppet::Type.newtype(:grafana_folder) do
   newparam(:uid) do
     desc 'UID of the folder'
   end
+
+  newparam(:grafana_url) do
+    desc 'The URL of the Grafana server'
+    defaultto ''
+
+    validate do |value|
+      unless value =~ %r{^https?://}
+        raise ArgumentError, format('%s is not a valid URL', value)
+      end
+    end
+  end
+
+  newparam(:grafana_user) do
+    desc 'The username for the Grafana server (optional)'
+  end
+
+  newparam(:grafana_password) do
+    desc 'The password for the Grafana server (optional)'
+  end
+
+  newparam(:grafana_api_path) do
+    desc 'The absolute path to the API endpoint'
+    defaultto '/api'
+
+    validate do |value|
+      unless value =~ %r{^/.*/?api$}
+        raise ArgumentError, format('%s is not a valid API path', value)
+      end
+    end
+  end
+
+  newparam(:organization) do
+    desc 'The organization name to create the datasource on'
+    defaultto 1
+  end
+
+  autorequire(:service) do
+    'grafana-server'
+  end
 end
